e2f8201339c6c0ed74e3cabbb994c6f3f8409f7a
Sargis <sargis.mkoyan97@gmail.com>
2022-03-16 21:58:24 +0400

    [db] add `Field` model

---
+++ api/models/field.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const fieldSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+}, {
+    collection: 'fields',
+    timestamps: true,
+});
+
+module.exports = mongoose.model('Fields', fieldSchema);
