3ad547eab443c4d52f89ad44ea0b12d9c80337cc
Sargis <sargis.mkoyan97@gmail.com>
2022-03-17 11:58:09 +0400

    [db] update `User` model
    add support for filtering users by name, surname, registration date and type (mentor|mentee)

--- api/models/user.js
+++ api/models/user.js
@@ -73,4 +73,32 @@ const userSchema = new Schema({
     timestamps: true,
 });
 
+
+userSchema.query.filter = function (ctx) {
+    if (typeof ctx.query.name !== 'undefined') {
+        // here RegExp used as an alternative to SQL LIKE operator
+        this.where('name').equals(new RegExp(`.*${ctx.query.name}.*`, 'i'));
+    }
+
+    if (typeof ctx.query.surname !== 'undefined') {
+        this.where('surname').equals(new RegExp(`.*${ctx.query.surname}.*`, 'i'));
+    }
+
+    if (typeof ctx.query.type !== 'undefined') {
+        this.where('type').equals(ctx.query.type);
+    }
+
+
+    if (typeof ctx.query.registeredAfter !== 'undefined') {
+        this.gte('createdAt', ctx.query.registeredAfter);
+    }
+
+    if (typeof ctx.query.registeredBefore !== 'undefined') {
+        this.lt('createdAt', ctx.query.ctx.query.registeredBefore);
+    }
+
+    return this;
+};
+
+
 module.exports = mongoose.model('User', userSchema);
